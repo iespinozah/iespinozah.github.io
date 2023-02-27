7b701770604dbc2f950ae2935c7741d849dc1cde
iespinozah <isabel.espinozah@gmail.com>
2023-02-27 03:08:32 -0300

    navbar y boton funciona, falta apellido

--- Jscripts.js
+++ Jscripts.js
@@ -1,7 +1,51 @@
 const boton = document.querySelector('#botoncito');
+const foto = document.querySelector('#foto');
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const correo = document.querySelector('#correo');
+const telefono = document.querySelector('#telefono');
 console.log("hola");
 
-const crearusuario=() =>{
-    console.log("generando usuario");
+const crearusuario = async() =>{
+    const url ='https://randomuser.me/api/';
+    const rpta= await fetch(url);
+    const { results } = await rpta.json();
+    const datos=results[0];
+    console.log(datos);
+    foto.src = datos.picture.medium;
+
+    nombre.textContent=datos.name.first; 
+    telefono.textContent = datos.phone;
+    correo.textContent = datos.email;
+    apellido.textContent=datos.name.last;
 }
 boton.addEventListener('click', crearusuario);
+document.addEventListener('DOMContentLoaded', crearusuario);
+document.getElementById("sobre").addEventListener('click', function(){
+    document.getElementById("sobremi").style.display="block";
+    document.getElementById("formacion").style.display="none";
+    document.getElementById("experiencia").style.display="none";
+    document.getElementById("habilidades").style.display="none";
+});
+document.getElementById("forma").addEventListener('click', function(){
+    document.getElementById("sobremi").style.display="none";
+    document.getElementById("formacion").style.display="block";
+    document.getElementById("experiencia").style.display="none";
+    document.getElementById("habilidades").style.display="none";
+});
+
+document.getElementById("xper").addEventListener('click', function(){
+    document.getElementById("sobremi").style.display="none";
+    document.getElementById("formacion").style.display="none";
+    document.getElementById("experiencia").style.display="block";
+    document.getElementById("habilidades").style.display="none";
+});
+
+document.getElementById("habil").addEventListener('click', function(){
+    document.getElementById("sobremi").style.display="none";
+    document.getElementById("formacion").style.display="none";
+    document.getElementById("experiencia").style.display="none";
+    document.getElementById("habilidades").style.display="block";
+});
+
+
